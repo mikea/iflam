ffb41f23ace6520b90a709dc194e455d18c2af4e
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-01-09 23:43:10 -0800

    Basic parsing done.

--- iflam/genome.cpp
+++ iflam/genome.cpp
@@ -8,12 +8,29 @@
 
 using std::string;
 using std::vector;
+using boost::scoped_ptr;
+
+
+Xform::Xform() { }
+
+Xform::~Xform() { }
 
 Genome::Genome() {
 }
 
 Genome::~Genome() {
 }
+
+// ----------------------------------------------------------------------------
+// - Parsing
+// ----------------------------------------------------------------------------
+
+typedef boost::error_info<struct tag_error_message, std::string> error_message;
+
+struct vector_parse_error : virtual parse_error { };
+struct xml_parse_error : virtual parse_error { };
+struct unsupported_attribute_error : virtual parse_error { };
+struct unsupported_element_error : virtual parse_error { };
 
 namespace {
   template<typename T, size_t size>
@@ -22,7 +39,10 @@ namespace {
     vector<string> strs;
     boost::split(strs, str, boost::is_space());
     if (strs.size() != size) {
-      throw ReadError("Wrong vector length in " + str);
+      BOOST_THROW_EXCEPTION(vector_parse_error()
+          << error_message("Wrong vector length in '" + str + "'. Is " +
+            boost::lexical_cast<string>(strs.size()) + " but should be " +
+            boost::lexical_cast<string>(size)));
     }
     for (size_t i = 0; i < strs.size(); ++i) {
       (*array)[i] = boost::lexical_cast<T>(strs[i]);
@@ -34,16 +54,123 @@ namespace {
     // TODO: error checks
     *d = boost::lexical_cast<T>(str);
   }
+
+  const char * const kVariationNames[Xform::kVariationsCount] = {
+    "linear",
+    "sinusoidal",
+    "spherical",
+    "swirl",
+    "horseshoe",
+    "polar",
+    "handkerchief",
+    "heart",
+    "disc",
+    "spiral",
+    "hyperbolic",
+    "diamond",
+    "ex",
+    "julia",
+    "bent",
+    "waves",
+    "fisheye",
+    "popcorn",
+    "exponential",
+    "power",
+    "cosine",
+    "rings",
+    "fan",
+    "blob",
+    "pdj",
+    "fan2",
+    "rings2",
+    "eyefish",
+    "bubble",
+    "cylinder",
+    "perspective",
+    "noise",
+    "julian",
+    "juliascope",
+    "blur",
+    "gaussian_blur",
+    "radial_blur",
+    "pie",
+    "ngon",
+    "curl",
+    "rectangles",
+    "arch",
+    "tangent",
+    "square",
+    "rays",
+    "blade",
+    "secant2",
+    "twintrian",
+    "cross",
+    "disc2",
+    "super_shape",
+    "flower",
+    "conic",
+    "parabola",
+    "bent2",
+    "bipolar",
+    "boarders",
+    "butterfly",
+    "cell",
+    "cpow",
+    "curve",
+    "edisc",
+    "elliptic",
+    "escher",
+    "foci",
+    "lazysusan",
+    "loonie",
+    "pre_blur",
+    "modulus",
+    "oscilloscope",
+    "polar2",
+    "popcorn2",
+    "scry",
+    "separation",
+    "split",
+    "splits",
+    "stripes",
+    "wedge",
+    "wedge_julia",
+    "wedge_sph",
+    "whorl",
+    "waves2",
+    "exp",
+    "log",
+    "sin",
+    "cos",
+    "tan",
+    "sec",
+    "csc",
+    "cot",
+    "sinh",
+    "cosh",
+    "tanh",
+    "sech",
+    "csch",
+    "coth",
+    "auger",
+    "flux",
+    "mobius",
+  };
+
 }
 
 void Genome::Read(string file_name) {
   TiXmlDocument doc(file_name.c_str());
   if (!doc.LoadFile()) {
-    throw ReadError("Can't load file: " + file_name);
+    // throw ReadError();
+    BOOST_THROW_EXCEPTION(xml_parse_error()
+        << error_message("Can't parse")
+        << boost::errinfo_file_name(file_name));
   }
 
   const TiXmlElement* root = doc.RootElement();
 
+  // parse attributes
   for (const TiXmlAttribute* attr = root->FirstAttribute();
        attr != NULL;
        attr = attr->Next()) {
@@ -104,10 +231,114 @@ void Genome::Read(string file_name) {
     } else if (attr_name == "notes") {
       notes_ = attr->ValueStr();
     } else {
-      throw ReadError("Unsupported attribute: " + attr_name + "=\"" +
-        attr->Value() + "\"");
+      BOOST_THROW_EXCEPTION(unsupported_attribute_error()
+          << error_message("Unsupported attribute: " + attr_name + "=\"" +
+            attr->Value() + "\""));
     }
   }
 
-  throw ReadError("Element parsing not yet implemented.");
+  // elements
+  for (const TiXmlElement* e = root->FirstChildElement();
+       e != NULL;
+       e = e->NextSiblingElement()) {
+    const string element_name(e->ValueStr());
+
+    if (element_name == "edit") {
+      // ignore this
+    } else if (element_name == "xform") {
+      std::auto_ptr<Xform> xform(new Xform());
+      xform->Parse(e);
+      xforms_.push_back(xform.release());
+    } else if (element_name == "finalxform") {
+      scoped_ptr<Xform> xform(new Xform());
+      xform->Parse(e);
+      swap(xform, final_xform_);
+    } else if (element_name == "color") {
+      size_t index = 0;
+      boost::array<double, 3> color;
+
+      for (const TiXmlAttribute* attr = e->FirstAttribute();
+          attr != NULL;
+          attr = attr->Next()) {
+        const string attr_name(attr->Name());
+
+        if (attr_name == "index") {
+          ParseScalar(attr->ValueStr(), &index);
+        } else if (attr_name == "rgb") {
+          ParseArray<double, 3>(attr->ValueStr(), &color);
+        } else {
+          BOOST_THROW_EXCEPTION(unsupported_attribute_error()
+              << error_message("Unsupported attribute: " + attr_name + "=\"" +
+                attr->Value() + "\""));
+        }
+      }
+
+      if (index > colors_.size()) {
+        BOOST_THROW_EXCEPTION(parse_error()
+            << error_message("Color index too big: " +
+              boost::lexical_cast<string>(index)));
+      }
+      colors_[index] = color;
+    } else {
+      BOOST_THROW_EXCEPTION(unsupported_element_error() 
+          << error_message("Unsupported element: " + element_name));
+    }
+  }
+}
+
+void Xform::Parse(const TiXmlElement* element) {
+  for (const TiXmlAttribute* attr = element->FirstAttribute();
+      attr != NULL;
+      attr = attr->Next()) {
+    const string attr_name(attr->Name());
+
+    if (attr_name == "coefs") {
+      ParseArray<double, 6>(attr->ValueStr(), &coefs_);
+    } else if (attr_name == "color") {
+      ParseScalar(attr->ValueStr(), &color_);
+    } else if (attr_name == "symmetry") {
+      double symmetry;
+      ParseScalar(attr->ValueStr(), &symmetry);
+      color_speed_ = (1 - symmetry) / 2;
+      animate_ = symmetry > 0 ? 0 : 1;
+    } else if (attr_name == "weight") {
+      ParseScalar(attr->ValueStr(), &weight_);
+    } else if (attr_name == "animate") {
+      ParseScalar(attr->ValueStr(), &animate_);
+    } else if (attr_name == "color_speed") {
+      ParseScalar(attr->ValueStr(), &color_speed_);
+    } else if (attr_name == "opacity") {
+      ParseScalar(attr->ValueStr(), &opacity_);
+    } else if (attr_name == "julian_dist") {
+      ParseScalar(attr->ValueStr(), &julian_dist_);
+    } else if (attr_name == "julian_power") {
+      ParseScalar(attr->ValueStr(), &julian_power_);
+    } else if (attr_name == "perspective_dist") {
+      ParseScalar(attr->ValueStr(), &perspective_dist_);
+    } else if (attr_name == "perspective_angle") {
+      ParseScalar(attr->ValueStr(), &perspective_angle_);
+    } else if (attr_name == "radial_blur_angle") {
+      ParseScalar(attr->ValueStr(), &radial_blur_angle_);
+    } else if (attr_name == "rings2_val") {
+      ParseScalar(attr->ValueStr(), &rings2_val_);
+    } else if (attr_name == "post") {
+      post_.reset(new boost::array<double, 6>);
+      ParseArray<double, 6>(attr->ValueStr(), post_.get());
+    } else {
+      bool found = false;
+      for (size_t i = 0; i < variations_.size(); ++i) {
+        if (kVariationNames[i] == attr_name) {
+          ParseScalar(attr->ValueStr(), &variations_[i]);
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        BOOST_THROW_EXCEPTION(unsupported_attribute_error()
+            << error_message("Unsupported attribute: " + attr_name + "=\"" +
+              attr->Value() + "\""));
+      }
+    }
+  }
 }
--- iflam/genome.h
+++ iflam/genome.h
@@ -6,10 +6,37 @@
 #include <boost/scoped_ptr.hpp>
 #include <boost/utility.hpp>
 #include <boost/array.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/exception/all.hpp>
 
-class ReadError : public std::runtime_error {
+class TiXmlElement;
+
+struct parse_error : virtual boost::exception, virtual std::exception { };
+
+class Xform : boost::noncopyable {
   public:
-    ReadError(const std::string& message) : std::runtime_error(message) { }
+    enum { kVariationsCount = 99 };
+    Xform();
+    ~Xform();
+
+    void Parse(const TiXmlElement* element);
+
+  private:
+
+    boost::array<double, 6> coefs_;
+    boost::array<double, kVariationsCount> variations_;
+    double color_;
+    double color_speed_;
+    double animate_;  // is it bool?
+    double weight_;
+    double opacity_;
+    double julian_dist_;
+    double julian_power_;
+    double perspective_angle_;
+    double perspective_dist_;
+    double radial_blur_angle_;
+    double rings2_val_;
+    boost::scoped_ptr<boost::array<double, 6> > post_;
 };
 
 class Genome : boost::noncopyable {
@@ -22,12 +49,14 @@ public:
     // Throws ReadError.
     void Read(std::string file_name);
 private:
+    typedef boost::array<double, 3> Color;
 
     std::string name_;
     double time_;
     boost::array<int, 2> size_;
     boost::array<double, 2> center_;
     boost::array<double, 3> background_;
+    boost::array<Color, 256> colors_;
     double scale_;
     double rotate_;
     int supersample_;
@@ -50,6 +79,9 @@ private:
     std::string url_;
     std::string nick_;
     std::string notes_;
+
+    boost::ptr_vector<Xform> xforms_;
+    boost::scoped_ptr<Xform> final_xform_;
 };
 
 
