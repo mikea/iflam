8427edfc31bc800fbc0ebad373737c005edea7de
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-01-16 00:30:07 -0800

    collecting fft data

---
+++ iflam/iflamAppDelegate.mm
@@ -0,0 +1,95 @@
+//
+//  iflamAppDelegate.m
+//  iflam
+//
+//  Created by Mike Aizatskyi on 1/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "iflamAppDelegate.h"
+#import <CoreAudio/CoreAudio.h>
+#import <boost/assert.hpp>
+#import "fft/FFTBufferManager.h"
+
+struct IOProcData {
+  UInt32 frames;
+  FFTBufferManager* fftManager;
+  int32_t* buf;
+  double* accum;
+  UInt32 samples;
+};
+
+AudioDeviceID GetDefaultInputDevice() {
+    AudioDeviceID theAnswer = 0;
+    UInt32 theSize = sizeof(AudioDeviceID);
+    AudioObjectPropertyAddress theAddress = {
+      kAudioHardwarePropertyDefaultInputDevice,
+      kAudioObjectPropertyScopeGlobal,
+      kAudioObjectPropertyElementMaster};
+
+    BOOST_VERIFY(!AudioObjectGetPropertyData(
+    kAudioObjectSystemObject,
+          &theAddress,
+          0,
+          NULL,
+          &theSize,
+          &theAnswer));
+    return theAnswer;
+}
+
+OSStatus MyIOProc(AudioDeviceID           inDevice,
+                  const AudioTimeStamp*   inNow,
+                  const AudioBufferList*  inInputData,
+                  const AudioTimeStamp*   inInputTime,
+                  AudioBufferList*        outOutputData,
+                  const AudioTimeStamp*   inOutputTime,
+                  void*                   inClientData) {
+  NSLog(@"MyIOProc");
+  IOProcData* procData = (IOProcData*)inClientData;
+
+  if (procData->fftManager->NeedsNewAudioData()) {
+    procData->fftManager->GrabAudioData(inInputData);
+    NSLog(@"Grab %d", inInputData->mBuffers[0].mDataByteSize);
+  } else {
+    BOOST_ASSERT(procData->fftManager->HasNewAudioData());
+    procData->fftManager->ComputeFFT(procData->buf);
+
+    for (int i = 0; i < procData->frames / 2; ++i) {
+      procData->accum[i] += procData->buf[i];
+    }
+    procData->samples++;
+  }
+
+  return 0;
+}
+
+@implementation iflamAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  AudioDeviceID inDevice = GetDefaultInputDevice();
+  NSLog(@"Default input device: %d", inDevice);
+
+  IOProcData* procData = new IOProcData();
+  int frames = 4096;
+  procData->frames = frames;
+  procData->fftManager = new FFTBufferManager(frames);
+  procData->buf = new int32_t[frames / 2];
+  procData->accum = new double[frames / 2];
+  procData->samples = 0;
+  memset(procData->accum, 0, sizeof(double)* frames / 2);
+
+  AudioDeviceIOProcID theIOProcID = NULL;
+
+  BOOST_VERIFY(!AudioDeviceCreateIOProcID(inDevice, MyIOProc, procData, &theIOProcID));
+  BOOST_VERIFY(!AudioDeviceStart(inDevice, theIOProcID));
+  // theError = AudioDeviceStop(inDevice, theIOProcID);
+  // theError = AudioDeviceDestroyIOProcID(inDevice, theIOProcID);
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
