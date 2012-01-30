d921359c79bb7e965c7de5f59d170027eb5040d1
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-01-30 11:55:08 -0800

    added resize

--- iflam/gl/main.cpp
+++ iflam/gl/main.cpp
@@ -8,12 +8,36 @@ void renderScene(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glBegin(GL_TRIANGLES);
-  glVertex3f(-0.5,-0.5,0.0);
-  glVertex3f(0.5,0.0,0.0);
-  glVertex3f(0.0,0.5,0.0);
+  {
+    glVertex3f(-2,-2,-5.0);
+    glVertex3f(2,0.0,-5.0);
+    glVertex3f(0.0,2,-5.0);
+  }
   glEnd();
 
   glutSwapBuffers();
+
+}
+void changeSize(int w, int h) {
+  if(h == 0) {
+    h = 1;
+  }
+  float ratio = 1.0* w / h;
+
+  // Use the Projection Matrix
+  glMatrixMode(GL_PROJECTION);
+
+  // Reset Matrix
+  glLoadIdentity();
+
+  // Set the viewport to be the entire window
+  glViewport(0, 0, w, h);
+
+  // Set the correct perspective.
+  gluPerspective(45,ratio,1,1000);
+
+  // Get Back to the Modelview
+  glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,6 +50,7 @@ int main(int argc, char *argv[]) {
 
   // register callbacks
   glutDisplayFunc(renderScene);
+  glutReshapeFunc(changeSize);
 
   // enter GLUT event processing cycle
   glutMainLoop();
