dc9f1cf3df69617d7285536bef4c4b76aacb7cd2
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-01-30 23:10:04 -0800

    shader almost works

--- iflam/gl/main.cpp
+++ iflam/gl/main.cpp
@@ -3,11 +3,24 @@
 #else
 #include <GL/glut.h>
 #endif
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 #include "genome.h"
 #include "renderer.h"
 
+#define CHECK_GL_ERROR() \
+  do { \
+    GLenum __err = glGetError(); \
+    BOOST_VERIFY_MSG(__err == GL_NO_ERROR, (std::string("GL Error: ") + std::string((const char*)gluErrorString(__err))).c_str()); \
+  } while (0)
+
+
 static GLuint texture_id;
+static GLuint fragment_shader_id;
+static GLuint vertex_shader_id;
+static GLuint program_id;
 
 class State {
   public:
@@ -88,6 +101,32 @@ void changeSize(int w, int h) {
   state = new State(w, h);
 }
 
+std::string LoadFile(const std::string& file_name) {
+  std::ifstream t(file_name.c_str());
+  BOOST_VERIFY(t.is_open());
+  std::string str(
+      (std::istreambuf_iterator<char>(t)),
+       std::istreambuf_iterator<char>());
+  return str;
+}
+
+void printShaderInfoLog(GLint shader) {
+  int infoLogLen = 0;
+  int charsWritten = 0;
+  GLchar *infoLog;
+
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLen);
+
+  if (infoLogLen > 0)
+  {
+    infoLog = new GLchar[infoLogLen];
+    glGetShaderInfoLog(shader, infoLogLen, &charsWritten, infoLog);
+    std::cout << infoLog << std::endl;
+    delete [] infoLog;
+  }
+}
+
+
 void init() {
 
   glClearColor (0.0, 0.0, 0.0, 0.0);
@@ -98,6 +137,65 @@ void init() {
 
   // create texture
   glGenTextures(1, &texture_id);
+
+  // setup shaders
+  {
+    GLint compiled;
+    std::string vertex = LoadFile("../iflam/gl/blur.vertex");
+    std::cout << "Vertex Shader: " << vertex;
+
+    vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
+    const char* ptr = vertex.c_str();
+    glShaderSource(vertex_shader_id, 1, &ptr, NULL);
+    glCompileShader(vertex_shader_id);
+    glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &compiled);
+    if(!compiled) {
+      printShaderInfoLog(vertex_shader_id);
+      BOOST_ASSERT(0);
+    }
+    CHECK_GL_ERROR();
+  }
+  {
+    GLint compiled;
+    std::string fragment = LoadFile("../iflam/gl/blur.fragment");
+    std::cout << "Fragment Shader: " << fragment;
+
+    fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* ptr = fragment.c_str();
+    glShaderSource(fragment_shader_id, 1, &ptr, NULL);
+    glCompileShader(fragment_shader_id);
+
+    glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &compiled);
+    if(!compiled) {
+      printShaderInfoLog(fragment_shader_id);
+      BOOST_ASSERT(0);
+    }
+    CHECK_GL_ERROR();
+  }
+
+  program_id = glCreateProgram();
+  CHECK_GL_ERROR();
+  glAttachShader(program_id, vertex_shader_id); 
+  CHECK_GL_ERROR();
+  glAttachShader(program_id, fragment_shader_id); 
+  CHECK_GL_ERROR();
+  glLinkProgram(program_id);
+  CHECK_GL_ERROR();
+  // Validate program
+  glValidateProgram(program_id);
+
+  // Check the status of the compile/link
+  int logLen;
+  glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &logLen);
+  if(logLen > 0) {
+    char* log = new char[logLen];
+    // Show any errors as appropriate
+    glGetProgramInfoLog(program_id, logLen, &logLen, log);
+    fprintf(stderr, "Prog Info Log: %s\n", log);
+    BOOST_ASSERT(0);
+  }
+  glUseProgram(program_id);
+  CHECK_GL_ERROR();
 }
 
 int main(int argc, char *argv[]) {
