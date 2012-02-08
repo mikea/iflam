05776727740bee9186277a05cef3a81106fb2675
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-02-07 17:35:11 -0800

    fully functional shader compilation

--- iflam/gl/render.cpp
+++ iflam/gl/render.cpp
@@ -5,14 +5,17 @@
 
 class vec3 {
   public:
-    vec3(float x1, float x2, float x3) { }
+    vec3(float x1, float x2, float x3) { 
+      value[0] = x1;
+      value[1] = x2;
+      value[2] = x3;
+    }
 
     float& operator[](int idx) { return value[idx]; }
   private:
-    float* value;
+    float value[3];
 };
 
-class Render {
+class RenderShader {
 #include "render.fragment"
 };
-
