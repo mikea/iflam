642a744b61c67d1c25150088decb5aa69ac56559
Mike Aizatsky <mike.aizatsky@gmail.com>
2012-02-28 17:22:25 -0800

    Successfully render FBO. Some problems with colors, though.

--- flam-java/src/flam/fgl/Main.java
+++ flam-java/src/flam/fgl/Main.java
@@ -3,7 +3,6 @@ package flam.fgl;
 import com.jogamp.opengl.util.FBObject;
 import com.jogamp.opengl.util.FPSAnimator;
 
-import javax.media.opengl.DebugGL2;
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
@@ -16,31 +15,20 @@ import javax.swing.*;
 /**
  */
 public class Main implements GLEventListener {
+// ------------------------------ FIELDS ------------------------------
     private double theta = 0;
     private double s = 0;
     private double c = 0;
     private FBObject fbo;
 
-    public static void main(String[] args) {
-        GLProfile glp = GLProfile.getDefault();
-        GLCapabilities caps = new GLCapabilities(glp);
-        GLCanvas canvas = new GLCanvas(caps);
-        canvas.addGLEventListener(new Main());
+// ------------------------ INTERFACE METHODS ------------------------
 
-        FPSAnimator animator = new FPSAnimator(canvas, 60);
-        animator.add(canvas);
-        animator.start();
 
-        JFrame frame = new JFrame("A window");
-        frame.add(canvas);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+// --------------------- Interface GLEventListener ---------------------
 
     @Override
     public void init(GLAutoDrawable drawable) {
-        drawable.setGL(new DebugGL2(drawable.getGL().getGL2()));
+//        drawable.setGL(new DebugGL2(drawable.getGL().getGL2()));
     }
 
     @Override
@@ -52,11 +40,27 @@ public class Main implements GLEventListener {
     @Override
     public void display(GLAutoDrawable drawable) {
         update();
-        render(drawable);
+        render(drawable.getGL().getGL2());
     }
 
-    private void render(GLAutoDrawable drawable) {
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL2 gl = drawable.getGL().getGL2();
+        if (fbo != null) {
+            fbo.destroy(gl);
+        }
+        fbo = new FBObject(width, height);
+        fbo.init(gl);
+        fbo.attachTexture2D(gl, 0, GL2.GL_NEAREST, GL2.GL_NEAREST, 0, 0);
+        if (!fbo.isStatusValid()) {
+            throw new IllegalStateException();
+        }
+        fbo.unbind(gl);
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    private void render(GL2 gl) {
         {
             fbo.bind(gl);
             renderScene(gl);
@@ -66,32 +70,14 @@ public class Main implements GLEventListener {
         renderFBO(gl);
     }
 
-    private void renderScene(GL2 gl) {
+    private void renderFBO(GL2 gl) {
+        gl.glEnable(GL.GL_TEXTURE_2D);
+        fbo.use(gl, 0);
         gl.glClearColor(0, 0, 0, 1);
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
         {
-            gl.glBegin(GL.GL_TRIANGLES);
-            gl.glColor3f(1, 0, 0);
-//            gl.glVertex2d(-c, -c);
-            gl.glVertex2d(0, 0);
-
-            gl.glColor3f(0, 1, 0);
-//            gl.glVertex2d(0, c);
-            gl.glVertex2d(0, 1);
-
-            gl.glColor3f(0, 0, 1);
-            gl.glVertex2d(s, -s);
-
-            gl.glEnd();
-        }
-    }
-
-    private void renderFBO(GL2 gl) {
-        gl.glEnable(GL.GL_TEXTURE_2D);
-        fbo.use(gl, 0);
-        gl.glLoadIdentity();
-        {
+            gl.glColor3f(1, 1, 1);
             gl.glBegin(GL2.GL_QUADS);
             gl.glTexCoord2d(0.0, 0.0);
             gl.glVertex2d(-1.0, -1.0);
@@ -110,24 +96,50 @@ public class Main implements GLEventListener {
         gl.glDisable(GL.GL_TEXTURE_2D);
     }
 
+    private void renderScene(GL2 gl) {
+        gl.glClearColor(.1f, .1f, .1f, 1);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+        gl.glLoadIdentity();
+        {
+            gl.glBegin(GL.GL_TRIANGLES);
+            gl.glColor3f(1, 0, 0);
+            gl.glVertex2d(-c, -c);
+//            gl.glVertex2d(0, 0);
+
+            gl.glColor3f(0, 0, 1);
+            gl.glVertex2d(0, c);
+//            gl.glVertex2d(0, 1);
+
+            gl.glColor3f(0, 1, 0);
+            gl.glVertex2d(s, -s);
+//            gl.glVertex2d(1, 0);
+
+            gl.glEnd();
+        }
+    }
+
     private void update() {
         theta += 0.01;
         s = Math.sin(theta);
         c = Math.cos(theta);
     }
 
-    @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-        DebugGL2 gl = (DebugGL2) drawable.getGL().getGL2();
-        if (fbo != null) {
-            fbo.destroy(gl);
-        }
-        fbo = new FBObject(width, height);
-        fbo.init(gl);
-        fbo.attachTexture2D(gl, 0, GL.GL_RGBA8, GL2.GL_RGBA, GL2.GL_UNSIGNED_INT_8_8_8_8_REV, GL2.GL_LINEAR, GL2.GL_LINEAR, 0, 0);
-        if (!fbo.isStatusValid()) {
-            throw new IllegalStateException();
-        }
-        fbo.unbind(gl);
+// --------------------------- main() method ---------------------------
+
+    public static void main(String[] args) {
+        GLProfile glp = GLProfile.getDefault();
+        GLCapabilities caps = new GLCapabilities(glp);
+        GLCanvas canvas = new GLCanvas(caps);
+        canvas.addGLEventListener(new Main());
+
+        FPSAnimator animator = new FPSAnimator(canvas, 60);
+        animator.add(canvas);
+        animator.start();
+
+        JFrame frame = new JFrame("A window");
+        frame.add(canvas);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
